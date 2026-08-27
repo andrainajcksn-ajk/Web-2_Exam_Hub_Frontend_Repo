3406afa62c9275ae67f9efc8928f84c1b09b2acc
RAHARISETA Tsiresy Riantsoa <tsiresyriantsoa3@gmail.com>
2026-08-27 20:37:42 +0300

    feat: deletion of folder exam-hub

--- exam-hub/src/api/authApi.js
+++
@@ -1,5 +0,0 @@
-import { apiClient } from './client.js'
-
-export function login(email, password) {
-  return apiClient.post('/auth/login', { email, password })
-}
--- exam-hub/src/api/resultsApi.js
+++
@@ -1,5 +0,0 @@
-import { apiClient } from './client.js'
-
-export function getExamResults(examId) {
-  return apiClient.get(`/exams/${examId}/results`)
-}
