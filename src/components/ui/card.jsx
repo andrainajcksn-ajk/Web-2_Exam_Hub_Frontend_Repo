715b71528ea063c932f3afba8fab2d51c6ec2f08
RAHARISETA Tsiresy Riantsoa <tsiresyriantsoa3@gmail.com>
2026-08-28 15:50:51 +0300

    feat: deletion of loadingPage and card

--- src/components/ui/card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children, className = '' }) {
-  return (
-    <div className={`bg-neutral-900 border border-neutral-800 rounded-xl p-6 ${className}`}>
-      {children}
-    </div>
-  );
-}
